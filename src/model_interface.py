ad21279039404e015d495225b4a1a2f1185b089a
scholar <ssegawapatrick994@gmail.com>
2025-03-14 19:35:27 +0300

    updated to supported openai chat completion method

--- src/model_interface.py
+++ src/model_interface.py
@@ -1,45 +1,41 @@
-import openai
 from abstractclasses.abstract_model_interface import AbstractModelInterface
-import anthropic
 from anthropic import Anthropic
 from typing import Optional
-
-import openai
+from openai import OpenAI
+import constants
 
 class GPTModelInterface(AbstractModelInterface):
     def __init__(self, openai_api_key: str):
-        openai.api_key = openai_api_key
+        self.client = OpenAI(api_key=openai_api_key)  
 
-    def send_to_model(self, system_prompt: str, task: str) -> str:
+    def send_to_model(self, system_prompt: str, task: str) -> str:       
         try:
-            response = openai.ChatCompletion.create(
+            response = self.client.chat.completions.create(
                 model="gpt-4",
                 messages=[
                     {"role": "system", "content": system_prompt},
                     {"role": "user", "content": task}
                 ],
-                max_tokens=8000,
-                temperature=0.5,
+                max_tokens= constants.MAX_GPT_TOKENS,
+                temperature= constants.GPT_TEMPERATURE,
                 n=1,
                 stop=None
             )
-            return response["choices"][0]["message"]["content"].strip()
+            return response.choices[0].message.content.strip()
         except Exception as e:
             print(f"Error while communicating with OpenAI API: {e}")
-            return ""
-
-
+            return None
 
 class CLDModelInterface(AbstractModelInterface):
     def __init__(self, anthropic_api_key: str):
-        self.client = anthropic.Anthropic(api_key=anthropic_api_key)
+        self.client = Anthropic(api_key=anthropic_api_key)
 
     def send_to_model(self, system_prompt: str, task: str) -> Optional[str]:
         try:
             response = self.client.messages.create(
                 model="claude-3-opus-20240229",
-                max_tokens=200000,
-                temperature=0.5,
+                max_tokens= constants.MAX_CLAUDE_TOKENS,
+                temperature= constants.CLAUDE_TEMPERATURE,
                 messages=[
                     {"role": "system", "content": system_prompt},
                     {"role": "user", "content": task}
