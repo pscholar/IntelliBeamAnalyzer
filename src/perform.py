5e8bf051f1788d9ee91481f1b25f0a9224cece8d
scholar <ssegawapatrick994@gmail.com>
2025-03-14 19:37:14 +0300

    added robust None returns from called functions; shifted creation of model objects to the gui.py file

--- src/perform.py
+++ src/perform.py
@@ -1,31 +1,31 @@
 
 import os
 from task_packager import TaskPackager
-from model_interface import GPTModelInterface 
-from model_interface import CLDModelInterface
 from response_retriever import BackTicksResponseRetriever 
 from instruction_runner import SimpleInstructionRunner
 import constants
 
-def perform_analysis(task, LLM = constants.DEFAULT_LLM):
+def perform_analysis(task, model = None):
+    if model is None:
+      return None
     TP = TaskPackager()
-    GPT = GPTModelInterface()
-    CLD = CLDModelInterface()
     BTR = BackTicksResponseRetriever()
     SIP = SimpleInstructionRunner()
     sys_prompt, task = TP.package_task(user_input=task,task_type="analysis")
-    if LLM == constants.GPT_LLM:
-      response = GPT.send_to_model(system_prompt=sys_prompt,task=task)
-    elif LLM == constants.CLAUDE_LLM:
-       response = CLD.send_to_model(system_prompt=sys_prompt,task=task)   
-    code = BTR.parse_response(response=response)    
+    response = model.send_to_model(system_prompt=sys_prompt,task=task)  
+    if response is None:
+       return None 
+    code = BTR.parse_response(response=response)
+    if code is None:
+       return None    
     results = SIP.save_and_run(code=code,output_file=constants.OUTPUTFILE)
     sys_prompt, task = TP.package_task(user_input=results,task_type="report")
-    if LLM == constants.GPT_LLM:
-      response = GPT.send_to_model(system_prompt=sys_prompt,task=task)
-    elif LLM == constants.CLAUDE_LLM:
-      response = CLD.send_to_model(system_prompt=sys_prompt,task=task)
+    response = model.send_to_model(system_prompt=sys_prompt,task=task)
+    if response is None:
+       return None 
     report = BTR.parse_response(response=response)
+    if code is None:
+       return None
     os.makedirs(os.path.dirname(constants.REPORTFILE), exist_ok=True)
     try:
       with open(constants.REPORTFILE, 'w') as file:
