63a7a7575d28b664bfc4020f4b5038d3f9cc9c48
scholar <ssegawapatrick994@gmail.com>
2025-10-22 10:54:59 +0300

    cleaned up folder

--- resources/GPT-4o/zero_shot_answers/answer_17_nom.py
+++
@@ -1,8 +0,0 @@
-from indeterminatebeam.loading import PointLoadV, UDLV
-from indeterminatebeam.indeterminatebeam import Support, Beam
-from utility.beam_analyzer import analyzer
-
-beam = Beam(4)
-beam.add_supports(Support(0, (1,1,1)))
-beam.add_loads(UDLV(-3000, (0,4)), PointLoadV(-8000, 4))
-analyzer(beam)
--- resources/GPT-4o/zero_shot_answers/generate.py
+++
@@ -1,3 +0,0 @@
-for i in range(1, 91):
-    with open(f"answer_{i}_adv.py", "w") as file:
-        file.write("# This is a placeholder file for answer_{i}_adv.py\n")
